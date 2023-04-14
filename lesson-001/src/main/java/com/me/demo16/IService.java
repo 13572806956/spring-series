276d4129369dfb38b4bb47e3dcf82a2592daa469
guoxiaowei <597509924@qq.com>
2023-04-14 16:05:42 +0800

    Java代理

---
+++ lesson-001/src/main/java/com/me/demo16/IService.java
@@ -0,0 +1,7 @@
+package com.me.demo16;
+
+public interface IService {
+    void m1();
+    void m2();
+    void m3();
+}
---
+++ lesson-001/src/main/java/com/me/demo16/IUserService.java
@@ -0,0 +1,10 @@
+package com.me.demo16;
+
+public interface IUserService {
+    /**
+     * 插入用户信息
+     *
+     * @param name
+     */
+    void insert(String name);
+}
