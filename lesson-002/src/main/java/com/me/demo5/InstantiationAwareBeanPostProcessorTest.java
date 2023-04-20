9212240292d9f5e6eb6b299ad220e7dff393b166
guoxiaowei <597509924@qq.com>
2023-04-20 19:03:38 +0800

    Bean的生命周期

---
+++ lesson-002/src/main/java/com/me/demo5/InstantiationAwareBeanPostProcessorTest.java
@@ -0,0 +1,32 @@
+package com.me.demo5;
+
+import com.me.demo1.Car;
+import org.junit.Test;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.InstantiationAwareBeanPostProcessor;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+
+public class InstantiationAwareBeanPostProcessorTest {
+
+    @Test
+    public void test1() {
+        DefaultListableBeanFactory factory = new DefaultListableBeanFactory();
+
+        //添加一个BeanPostProcessor：InstantiationAwareBeanPostProcessor
+        factory.addBeanPostProcessor(new InstantiationAwareBeanPostProcessor() {
+            @Override
+            public Object postProcessBeforeInstantiation(Class<?> beanClass, String beanName) throws BeansException {
+                System.out.println("调用postProcessBeforeInstantiation()");
+                //发现类型是Car类型的时候，硬编码创建一个Car对象返回
+                if (beanClass.isAssignableFrom(Car.class)) {
+                    Car car = new Car();
+                    car.setName("保时捷");
+                    return car;
+                }
+                return null;
+            }
+        });
+
+
+    }
+}
